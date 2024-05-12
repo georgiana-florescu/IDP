2ab1360acbddf17f24f6476b6dcd55ecde5ad822
Georgiana Florescu <georgiana.florescu@stud.acs.upb.ro>
2024-05-12 22:25:10 +0300

    fix: db creds

--- io_service/database.py
+++ io_service/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session, relationship
 from passlib.context import CryptContext
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://myuser:mypassword@datacollect_db:5432/mydatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://myuser:mypass@datacollect_db:5432/mydb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
